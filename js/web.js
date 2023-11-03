d8785482f259044673bca9292ee1aeae4e9ef5cc
joons5967 <dlwnstjd5967>
2023-11-03 17:57:31 +0900

    A형 끝

--- js/web.js
+++ js/web.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    setInterval(function(){
+        $(".swiper-wrapper").animate({"marginTop": -300 }, 400, function(){
+        $("div:first-child",this).appendTo($(this))
+        $(this).css("marginTop", 0)
+    })
+}, 3000)
+   
     //화면이 준비되면 실행해라.
 
 })
